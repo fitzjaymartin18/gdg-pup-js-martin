71ef6808857c299acb7f08a5ccfc9d5039e67118
fitzjaymartin_18 <jaymarnitram08@gmail.com>
2025-02-27 14:48:49 +0800

    Finish js8

--- Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
+++ Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
@@ -1,1 +1,19 @@
 // your code starts here
+document.addEventListener('DOMContentLoaded', function() {
+
+    const counterText = document.querySelector('#counter-text');
+    const increaseBtn = document.querySelector('#button-add');
+    const decreaseBtn = document.querySelector('#button-subtract');
+
+    let counter = 0;
+    increaseBtn.onclick = () => {
+        counter++;
+        counterText.innerHTML = counter;
+    }
+
+    decreaseBtn.onclick = () => {
+        counter--;
+        counterText.innerHTML = counter;
+    }
+});
+
